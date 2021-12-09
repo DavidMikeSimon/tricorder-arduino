3a849b192bcc728f9dd135f54f1aeed33d1726bf
David Simon <david.simon@color.com>
2021-12-08 22:36:08 -0800

    Request improvements, working sleep

--- tricorder-d1-mini.ino
+++ tricorder-d1-mini.ino
@@ -6,7 +6,7 @@
 
 #define PISO_SH_LD_PIN 5 // Set high to read from 74HC165
 #define POSI_BSRCLR_PIN 2 // Set high to write to 74HC595 shift registers
-#define POSI_RCLK_PIN 16 // Positive edge copies 74HC595 shift registers to output
+#define POSI_RCLK_PIN 15 // Positive edge copies 74HC595 shift registers to output
 
 #define WIDTH 240
 #define HEIGHT 320
@@ -19,6 +19,8 @@ TFT_eSprite spr = TFT_eSprite(&tft);
 WiFiClient client;
 
 void setup() {
+  enableWiFiAtBootTime();
+
   pinMode(PISO_SH_LD_PIN, OUTPUT);
   digitalWrite(PISO_SH_LD_PIN, LOW);
 
@@ -30,7 +32,8 @@ void setup() {
 
   Serial.begin(9600);
   //Serial.setDebugOutput(true);
-  
+
+  WiFi.persistent(true);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   tft.init();
@@ -44,12 +47,11 @@ void setup() {
 
   {
     TFT_eSprite spr2 = TFT_eSprite(&tft);
-    spr2.createSprite(240, 50);
-    spr2.setTextDatum(MC_DATUM);
+    spr2.createSprite(120, 25);
     spr2.setTextColor(TFT_WHITE, TFT_BLUE);
     spr2.fillSprite(TFT_BLUE);
-    spr2.drawString("I Love Pip!", 120, 25, 4);
-    spr2.pushSprite(0, 250);
+    spr2.drawString("I Love Pip!", 0, 0, 4);
+    spr2.pushSprite(60, 250);
   }
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -83,43 +85,49 @@ void loop() {
   delay(1);
   digitalWrite(POSI_BSRCLR_PIN, LOW);
 
-  String buttonStatus = String(input, BIN);
+  String buttonStatus = input == 0 ? "RDY" : String(input, BIN);
   spr.fillSprite(TFT_BLUE);
   spr.drawString(buttonStatus, 120, 25, 4);
 
   spr.pushSprite(0, 150);
 
   byte pressed = input & ~lastInput;
-  if (pressed > 0) {
-    setSwitch("switch.corner_lamp", pressed == 0b1000);
+  if (pressed == 0b1000) {
+    setSwitch("switch.corner_lamp", true);
+  } else if (pressed == 0b100) {
+    setSwitch("switch.corner_lamp", false);
+  } else if (pressed == 0b10) {
+    Serial.print("Before sleep");
+    ESP.deepSleep(3e6);
   }
   lastInput = input;
 
   delay(50);
 }
 
-char netBuffer[512];
-
-void setSwitch(String entityName, bool targetState) {
-  int requestLen = sprintf(
-    netBuffer,
-    "POST /api/services/switch/turn_%s HTTP/1.0\r\n"
-      "Content-Type: application/json\r\n"
-      "Content-Length: %u\r\n"
-      "Authorization: Bearer %s\r\n"
-      "\r\n"
-      "{\"entity_id\":\"%s\"}\r\n"
-      "\r\n",
-    targetState ? "on" : "off",
-    entityName.length() + 16, // FIXME stupid way to do this
-    HOME_ASSISTANT_API_KEY,
-    entityName.c_str()
+void setSwitch(const char* entityName, bool targetState) {
+  char reqBodyBuffer[256];
+  int reqBodyLen = sprintf(
+    reqBodyBuffer,
+    "{\"entity_id\":\"%s\"}\r\n\r\n",
+    entityName
   );
 
   if (!client.connect(HOME_ASSISTANT_HOST, HOME_ASSISTANT_PORT)) {
     Serial.println("Error connecting");
     return;
   }
-  client.write(netBuffer, requestLen);
+
+  client.printf(
+   "POST /api/services/switch/turn_%s HTTP/1.0\r\n"
+      "Content-Type: application/json\r\n"
+      "Content-Length: %u\r\n"
+      "Authorization: Bearer %s\r\n"
+      "\r\n",
+    targetState ? "on" : "off",
+    reqBodyLen,
+    HOME_ASSISTANT_API_KEY
+  );
+  client.write(reqBodyBuffer, reqBodyLen);
   client.stop();
 }
