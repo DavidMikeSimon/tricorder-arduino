1003664b0882ca1df3a9d3bfb240980862745291
David Simon <david.simon@color.com>
2022-02-11 16:58:45 -0800

    WIP

--- tricorder-d1-mini.ino
+++ tricorder-d1-mini.ino
@@ -3,14 +3,15 @@
 #include <ESP8266WiFi.h>
 #include <AudioFileSourcePROGMEM.h>
 #include <AudioGeneratorWAV.h>
-#include <AudioOutputI2SNoDAC.h>
+#include <AudioOutputI2S.h>
 
 #include "secrets.h"
+#include "sounds/tng_tricorder_close.wav.h"
+#include "sounds/tng_tricorder_open.wav.h"
 #include "sounds/tng_tricorder_scan.wav.h"
+#include "sounds/tng_tricorder_scan_low_beep.wav.h"
 
-#define PISO_SH_LD_PIN 5 // Set high to read from 74HC165
-#define POSI_BSRCLR_PIN 2 // Set high to write to 74HC595 shift registers
-#define POSI_RCLK_PIN 15 // Positive edge copies 74HC595 shift registers to output
+#define PAR_SELECT_PIN 5 // Set high to write to 74HC595 and read from 74HC165
 
 #define WIDTH 240
 #define HEIGHT 320
@@ -24,16 +25,72 @@ WiFiClient client;
 
 AudioFileSourcePROGMEM* audioFile;
 AudioGeneratorWAV* wav;
-AudioOutputI2SNoDAC* audioOut;
+AudioOutputI2S* audioOut;
+
+byte parallelOutput = 0;
+byte parallelInput1 = 0;
+byte parallelInput2 = 0;
+
+void readWriteParallel() {
+  digitalWrite(PAR_SELECT_PIN, HIGH);
+
+  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE3));
+  // The 74HC165 shifts the first bit _after_ the first clock cycle,
+  // so the first bit we read is useless. Here we are just discarding
+  // the highest-order bit of each byte, but we could get them if we wanted
+  // by looking at the lowest-order bit of the following byte.
+  parallelInput1 = SPI.transfer(0) >> 1;
+  parallelInput2 = SPI.transfer(parallelOutput) >> 1;
+  SPI.endTransaction();
+
+  digitalWrite(PAR_SELECT_PIN, LOW);
+}
+
+void setSwitch(const char* entityName, bool targetState) {
+  char reqBodyBuffer[256];
+  int reqBodyLen = sprintf(
+                     reqBodyBuffer,
+                     "{\"entity_id\":\"%s\"}\r\n\r\n",
+                     entityName
+                   );
+
+  if (!client.connect(HOME_ASSISTANT_HOST, HOME_ASSISTANT_PORT)) {
+    return;
+  }
+
+  client.printf(
+    "POST /api/services/switch/turn_%s HTTP/1.0\r\n"
+    "Content-Type: application/json\r\n"
+    "Content-Length: %u\r\n"
+    "Authorization: Bearer %s\r\n"
+    "\r\n",
+    targetState ? "on" : "off",
+    reqBodyLen,
+    HOME_ASSISTANT_API_KEY
+  );
+  client.write(reqBodyBuffer, reqBodyLen);
+  client.stop();
+}
 
 void setup() {
   enableWiFiAtBootTime();
 
+  pinMode(PAR_SELECT_PIN, OUTPUT);
+  digitalWrite(PAR_SELECT_PIN, LOW);
+
+  audioFile = new AudioFileSourcePROGMEM();
+  audioOut = new AudioOutputI2S();
+  wav = new AudioGeneratorWAV();
+
   Serial.begin(9600);
-  //Serial.setDebugOutput(true);
+  Serial.setDebugOutput(true);
+
+//  audioFile->open(tng_tricorder_open_wav, sizeof(tng_tricorder_open_wav));
+//  wav->begin(audioFile, audioOut);
+//  wav->loop();
 
   WiFi.persistent(true);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  //WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   tft.init();
   tft.setRotation(2);
@@ -49,132 +106,109 @@ void setup() {
 
   spr.createSprite(240, 50);
   spr.setTextDatum(MC_DATUM);
-  spr.setTextColor(TFT_WHITE, TFT_BLUE);
-  
-  tft.fillScreen(TFT_BLUE);
+  spr.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  tft.fillScreen(TFT_BLACK);
+
+  parallelOutput = 0b10101;
+  readWriteParallel();
 
   {
     TFT_eSprite spr2 = TFT_eSprite(&tft);
     spr2.createSprite(120, 25);
-    spr2.setTextColor(TFT_WHITE, TFT_BLUE);
-    spr2.fillSprite(TFT_BLUE);
+    spr2.setTextColor(TFT_WHITE, TFT_BLACK);
+    spr2.fillSprite(TFT_BLACK);
     spr2.drawString("I Love Pip!", 0, 0, 4);
     spr2.pushSprite(60, 250);
   }
 
-  audioFile = new AudioFileSourcePROGMEM();
-  audioOut = new AudioOutputI2SNoDAC();
-  wav = new AudioGeneratorWAV();
-
-  pinMode(PISO_SH_LD_PIN, OUTPUT);
-  digitalWrite(PISO_SH_LD_PIN, LOW);
-
-  pinMode(POSI_BSRCLR_PIN, OUTPUT);
-  digitalWrite(POSI_BSRCLR_PIN, LOW);
-
-  pinMode(POSI_RCLK_PIN, OUTPUT);
-  digitalWrite(POSI_RCLK_PIN, LOW);
-
-  write595(0b10000);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(200);
-    write595(0b10101);
+  while (wav->isRunning()) {
+    if (!wav->loop()) {
+      wav->stop();
+    }
+    delay(1);
   }
+  
+  //while (WiFi.status() != WL_CONNECTED) {
+  //  delay(200);
+  //}
 
-  Serial.print("Heap space: ");
-  Serial.println(ESP.getFreeHeap());
-  Serial.println();
-  Serial.println();
-}
-
-
-void write595(byte value) {
-  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE3));
-  digitalWrite(POSI_BSRCLR_PIN, HIGH);
-  SPI.transfer(value);
-  SPI.endTransaction();
-
-  digitalWrite(POSI_RCLK_PIN, HIGH);
-  delay(1);
-  digitalWrite(POSI_RCLK_PIN, LOW);
-  delay(1);
-  digitalWrite(POSI_BSRCLR_PIN, LOW);
+  //  Serial.print("Heap space: ");
+  //  Serial.println(ESP.getFreeHeap());
+  //  Serial.println();
+  //  Serial.println();
 }
 
 byte lastInput = 0;
 
 void loop() {
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE2));
-  digitalWrite(PISO_SH_LD_PIN, HIGH);
-  byte input1 = SPI.transfer(0);
-  byte input2 = SPI.transfer(0);
-  digitalWrite(PISO_SH_LD_PIN, LOW);
-  SPI.endTransaction();
+  parallelOutput = lastInput | 0b10000;
+  readWriteParallel();
 
-  write595(input2 | 0b10000);
-
-  String buttonStatus = input1 == 0 ? "RDY" : String(input1, BIN);
-  spr.fillSprite(TFT_BLUE);
+  String buttonStatus = parallelInput1 == 0 ? "RDY" : String(parallelInput1, BIN);
+  spr.fillSprite(TFT_BLACK);
   spr.drawString(buttonStatus, 120, 25, 4);
   spr.pushSprite(0, 150);
-  buttonStatus = input2 == 0 ? "RDY" : String(input2, BIN);
-  spr.fillSprite(TFT_BLUE);
+  buttonStatus = parallelInput2 == 0 ? "RDY" : String(parallelInput2, BIN);
+  spr.fillSprite(TFT_BLACK);
   spr.drawString(buttonStatus, 120, 25, 4);
   spr.pushSprite(0, 200);
 
-  byte pressed = input2 & ~lastInput;
+  byte pressed = parallelInput2 & ~lastInput;
+  if (pressed != 0) {
+    Serial.println("PRESSED");
+    Serial.println(String(pressed, BIN));
+  }
   if (pressed == 0b1000) {
-    setSwitch("switch.corner_lamp", true);
+    //setSwitch("switch.corner_lamp", true);
   } else if (pressed == 0b100) {
-    setSwitch("switch.corner_lamp", false);
+    //setSwitch("switch.corner_lamp", false);
+    if (!wav->isRunning()) {
+      audioFile->open(tng_tricorder_scan_wav, sizeof(tng_tricorder_scan_wav));
+      wav->begin(audioFile, audioOut);
+    }
   } else if (pressed == 0b10) {
-    write595(0b100000); // Enable reset signal from reed switch
+    if (!wav->isRunning()) {
+      audioFile->open(tng_tricorder_scan_wav, sizeof(tng_tricorder_scan_wav));
+      wav->begin(audioFile, audioOut);
+    }
+  } else if (parallelInput2 == 0b0) {
+    if (wav->isRunning()) {
+      wav->stop();
+    }
+    audioFile->open(tng_tricorder_close_wav, sizeof(tng_tricorder_close_wav));
+    wav->begin(audioFile, audioOut);
+    while (wav->isRunning()) {
+      if (!wav->loop()) {
+        wav->stop();
+      }
+      delay(1);
+    }
+
+    readWriteParallel();
     tft.startWrite();
     tft.writecommand(TFT_SLPIN);
     tft.endWrite();
-    ESP.deepSleep(0);
-//  } else if (pressed == 0b1) {
-//    if (wav->isRunning()) {
-//      wav->stop();
-//    }
-//    audioFile->open(tng_tricorder_scan_wav, sizeof(tng_tricorder_scan_wav));
-//    wav->begin(audioFile, audioOut);
+    //ESP.deepSleep(0);
   }
-  lastInput = input2;
+
+  if (parallelInput1 == 0b1) {
+    //    if (wav->isRunning()) {
+    //      wav->stop();
+    //    }
+    if (!wav->isRunning()) {
+      audioFile->open(tng_tricorder_scan_low_beep_wav, sizeof(tng_tricorder_scan_low_beep_wav));
+      wav->begin(audioFile, audioOut);
+    }
+  }
+
+  lastInput = parallelInput2;
 
   if (wav->isRunning()) {
     if (!wav->loop()) {
       wav->stop();
-      Serial.println("WAV done");
     }
   }
 
   delay(1);
-}
-
-void setSwitch(const char* entityName, bool targetState) {
-  char reqBodyBuffer[256];
-  int reqBodyLen = sprintf(
-    reqBodyBuffer,
-    "{\"entity_id\":\"%s\"}\r\n\r\n",
-    entityName
-  );
-
-  if (!client.connect(HOME_ASSISTANT_HOST, HOME_ASSISTANT_PORT)) {
-    return;
-  }
-
-  client.printf(
-   "POST /api/services/switch/turn_%s HTTP/1.0\r\n"
-      "Content-Type: application/json\r\n"
-      "Content-Length: %u\r\n"
-      "Authorization: Bearer %s\r\n"
-      "\r\n",
-    targetState ? "on" : "off",
-    reqBodyLen,
-    HOME_ASSISTANT_API_KEY
-  );
-  client.write(reqBodyBuffer, reqBodyLen);
-  client.stop();
 }
